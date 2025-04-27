5c1f39fbd1307f272708f37187d4f07581d94275
Ian Morete <christianmorete3@gmail.com>
2025-04-27 14:42:58 +0800

    dfgh

--- App.py
+++ App.py
@@ -24,8 +24,8 @@ model = genai.GenerativeModel(model_name="models/gemini-1.5-flash-latest")
 # --- OAuth setup ---
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 google_bp = make_google_blueprint(
-    client_id=os.environ.get('GOOGLE_OAUTH_CLIENT_ID', '864440386085-bfogmij6ipnvrghpsvf0ahfuuhdfnu9u.apps.googleusercontent.com'),
-    client_secret=os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET', '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'),
+    client_id=os.environ.get('GOOGLE_OAUTH_CLIENT_ID', ''),
+    client_secret=os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET', ''),
     scope=[
         "openid",
         "https://www.googleapis.com/auth/userinfo.profile",
